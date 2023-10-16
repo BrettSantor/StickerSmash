178d0e6d175d2c6180a66fca1b97a068f639b160
BrettSantor <b.santor27@gmail.com>
2023-10-16 15:06:14 -0700

    add saving function and permissions request

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
-import { useState } from 'react';
+import * as MediaLibrary from 'expo-media-library';
+import { useState, useRef } from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { captureRef } from 'react-native-view-shot';
 
 import ImageViewer from './components/customImageViewer';
 import Button from './components/customButton';
@@ -19,6 +21,12 @@ export default function App() {
   const [isModalVisible, setIsModalVisible] = useState(false);
 const [showAppOptions, setShowAppOptions]= useState(false);
   const [selectedImage, setSelectedImage] = useState(null);
+  const [status, requestPermission] = MediaLibrary.usePermissions();
+  const imageRef = useRef();
+
+  if (status === null) {
+    requestPermission();
+  };
 
 const pickImageAsync = async () => {
   let result = await ImagePicker.launchImageLibraryAsync({
@@ -46,15 +54,29 @@ const onModalClose = () => {
 };
 
 const onSaveImageAsync = async () => {
-  //please hold
+  try {
+    const localUri = await captureRef(imageRef, {
+      height: 440,
+      quality: 1,
+    });
+    await MediaLibrary.saveToLibraryAsync(localUri);
+    if (localUri) {
+      alert("Saved!");
+    }
+  } catch(e) {
+    console.log(e)
+    alert("Oh no...Something has gone wrong")
+  }
 };
 
 
   return (
     <GestureHandlerRootView style={styles.container}>
       <View style = {styles.imageContainer}>
+        <View ref={imageRef} collapsable={false}>
         <ImageViewer placeholderImageSource={PlaceholderImage} selectedImage={selectedImage}/>
         {pickedEmoji !== null ? <EmojiSticker imageSize={40} stickerSource={pickedEmoji}/> : null}
+        </View>
         </View>
         {showAppOptions ? (
       <View style={styles.optionsContainer}>
